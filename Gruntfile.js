9108ce92ebc0449cc83ae7017c12e9344afe6f3f
breadam <bilgi@breadam.com>
2015-01-11 18:46:55 +0200

    Signed-off-by: breadam <bilgi@breadam.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -114,7 +114,7 @@ module.exports = function (grunt) {
     },
     connect: {
       options: {
-        port: 9000,
+        port: process.env.PORT,
         // change this to '0.0.0.0' to access the server from outside
         hostname: 'localhost'
       },
